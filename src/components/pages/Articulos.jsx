ded44df76570ccdec204786fe469c1d7e69d3782
eldeividrig <davidgabrielb@hotmail.com>
2023-01-13 12:18:31 -0300

    articulos

--- src/components/pages/Articulos.jsx
+++ src/components/pages/Articulos.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Global } from "../../helpers/Global";
 
 export const Articulos = () => {
+  const [articulos, setArticulos] = useState([]);
+  useEffect(() => {
+    conseguirArticulos();
+  }, []);
+
+  const conseguirArticulos = async () => {
+    const url = Global.url + "articulos";
+    let peticion = await fetch(url, {
+      method: "GET",
+    });
+    let datos = await peticion.json();
+    if (datos.status === "success") {
+      setArticulos(datos.articulos);
+    }
+  };
+
   return (
     <>
-      <article className="articulo-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
+      {articulos.length >= 1 ? (
+        articulos.map((articulo) => {
+          return (
+            <article key={articulo._id} className="articulo-item">
+              <div className="mascara">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Cuesta_del_obispo_01.jpg" />
+              </div>
+              <div className="datos">
+                <h3 className="title">{articulo.titulo}</h3>
+                <p className="description">{articulo.contenido}</p>
 
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="articulo-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="articulo-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
-
-      <article className="articulo-item">
-        <h3 className="title">Desarrollo web</h3>
-        <p className="description">victorroblesweb.es</p>
-
-        <button className="edit">Editar</button>
-        <button className="delete">Borrar</button>
-      </article>
+                <button className="edit">Editar</button>
+                <button className="delete">Borrar</button>
+              </div>
+            </article>
+          );
+        })
+      ) : (
+        <h1>No hay articulos</h1>
+      )}
     </>
-  )
-}
+  );
+};
